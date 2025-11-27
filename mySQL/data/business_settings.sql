7beadbcddf2a6ac7717324c1a5030efd6abccd28
Riddhi <rxiddhi@gmail.com>
2025-11-27 19:42:33 +0530

    Add business settings SQL schema and sample data

---
+++ mySQL/data/business_settings.sql
@@ -0,0 +1,7 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- Data for table business_settings
+INSERT INTO `business_settings` (`id`, `business_name`, `address`, `phone`, `email`, `gst_number`, `tax_rate`, `currency`, `invoice_prefix`, `invoice_counter`, `logo_url`, `updated_at`) VALUES ('1', 'Kulhad Chai Restaurant', 'Shop No. 123, Main Street, City', '+91 9876543210', 'info@kulhadchai.shop', 'GST123456789', 0, 'INR', 'INV', 0, '', '2025-10-28T11:39:16.185765+00:00');
+
+
+SET FOREIGN_KEY_CHECKS = 1;
